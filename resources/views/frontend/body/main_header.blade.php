148d7a3542b7db1dbaafc7862ab9dad960469852
Lotfar Kaes <lutfurkayes@yahoo.com>
2024-10-12 23:48:27 +0200

    Test

--- resources/views/frontend/body/main_header.blade.php
+++ resources/views/frontend/body/main_header.blade.php
@@ -71,7 +71,7 @@
                                 </li>
 
                                     <li>
-                                        <a class="active" href="index.html">Home  </a>
+                                        <a class="active" href="index.html">Home from deal  </a>
                                         
                                     </li>
                                     <li>
